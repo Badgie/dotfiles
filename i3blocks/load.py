deed22f953bf75b561b3170832f7e19f89b72ce7
Badgy <jonaskhansen@gmail.com>
2019-10-09 20:16:46 +0200

    add color formatting

--- i3blocks/load.py
+++ i3blocks/load.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python
+import os
+
+
+def print_result(load, color):
+    os.system(f'echo " {load}"')
+    os.system(f'echo ')
+    os.system(f'echo "{color}"')
+
 
 load_file = open('/proc/loadavg')
 load_list = load_file.read().strip('\n').split(' ')
 load_file.close()
 
 # [1 min, 5 min, 10 min]
-print(f'LOAD: {load_list[0]}')
+if float(load_list[0]) > 2.5:
+    if float(load_list[0]) > 5.0:
+        print_result(load=load_list[0], color='#FA5858')
+    else:
+        print_result(load=load_list[0], color='#F4FA58')
+else:
+    print(f' {load_list[0]}')
