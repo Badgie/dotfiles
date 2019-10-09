582b6a5253694d442daab1e77c0d9f1f1a6bedaa
Badgy <jonaskhansen@gmail.com>
2019-10-09 20:16:17 +0200

    add color formatting

--- i3blocks/batt.py
+++ i3blocks/batt.py
@@ -2,9 +2,16 @@
 
 import subprocess
 import re
+import os
+
+
+def print_result(charge, color):
+    os.system(f'echo " {charge}%"')
+    os.system(f'echo ')
+    os.system(f'echo "{color}"')
+
 
 regex = r'\d+\.\d+'
-bat = 'BAT:'
 
 ps = subprocess.Popen(['upower', '-i', '/org/freedesktop/UPower/devices/battery_BAT0'], stdout=subprocess.PIPE)
 status = subprocess.run(['grep', 'energy'], stdin=ps.stdout, stdout=subprocess.PIPE).stdout.decode('utf-8')
@@ -12,8 +19,14 @@ status_list = re.split('\n', status)
 
 # [energy, energy-empty, energy-full, energy-full-design, energy-rate]
 try:
-    current = float(re.search(regex, status_list[0]).group(0))
-    full = float(re.search(regex, status_list[2]).group(0))
-    print(f'{bat} {round(current / full * 100, 2)}%')
+    current_charge = round(
+        float(re.search(regex, status_list[0]).group(0)) / float(re.search(regex, status_list[2]).group(0)) * 100, 2)
+    if current_charge < 50:
+        if current_charge < 20:
+            print_result(charge=current_charge, color='#FA5858')
+        else:
+            print_result(charge=current_charge, color='#F4FA58')
+    else:
+        print(f' {current_charge}%')
 except:
-    print(f'{bat} \u2620')
+    print(f' \u2620')
