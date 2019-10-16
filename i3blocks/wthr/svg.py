a274ef5530ee7d474875fbe0392264941a0be762
Badgy <jonaskhansen@gmail.com>
2019-10-16 19:22:54 +0200

    remove data mining scripts

--- i3blocks/wthr/svg.py
+++
@@ -1,8 +0,0 @@
-from urllib import request
-from urllib.error import URLError
-
-for x in range(0, 200):
-    try:
-        request.urlretrieve(f'https://www.dmi.dk/fileadmin/assets/img/{x}.svg', f'./svg/{x}.svg')
-    except URLError:
-        continue
