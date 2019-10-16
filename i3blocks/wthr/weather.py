0a961ab62b46cbe32ea74848f80e136a7d269561
Badgy <jonaskhansen@gmail.com>
2019-10-16 19:21:48 +0200

    add descriptor using symbols from data

--- i3blocks/wthr/weather.py
+++ i3blocks/wthr/weather.py
@@ -6,7 +6,6 @@ from urllib.error import URLError
 import subprocess
 
 city = int(subprocess.run(['python', './scrloc.py'], stdout=subprocess.PIPE).stdout.decode('utf-8'))
-# 103;overcast, 3;rain, 2;cloudy, 160;light rain, 102;cloudynight, 63;rainshowers, 80;cloudyrain, 180;rainycloudynight
 
 
 def get_weather_data(url: str) -> str:
@@ -29,23 +28,51 @@ def format_wind_dir(wind: str) -> str:
     return wind
 
 
-def format_weather_desc(prec: float) -> str:
-    if prec < 0.5:
-        return 'Drizzle'
-    elif prec < 1:
-        return 'Light rain'
-    elif prec < 2:
-        return 'Rain'
-    else:
-        return 'Heavy rain'
+def format_weather_desc(prec: float, icon: int) -> str:
+    if icon is 1:
+        return 'Sunny'
+    elif icon is 2 or 102 or 103:
+        return 'Cloudy'
+    elif icon is 3:
+        return 'Overcast'
+    elif icon is 60 or 80 or 160 or 180:
+        if prec < 0.5:
+            return 'Drizzle'
+        else:
+            return 'Light rain'
+    elif icon is 63 or 81 or 163 or 181:
+        if prec < 2:
+            return 'Rain'
+        else:
+            return 'Heavy rain'
+    elif icon is 68 or 83 or 168 or 183:
+        return 'Light sleet'
+    elif icon is 69 or 84 or 169 or 184:
+        return 'Sleet'
+    elif icon is 70 or 85 or 170 or 185:
+        return 'Light snow'
+    elif icon is 73 or 86 or 173 or 186:
+        return 'Snow'
+    elif icon is 95 or 195:
+        if prec < 0.5:
+            return 'Thunder and drizzle'
+        elif prec < 1:
+            return 'Thunder and light rain'
+        elif prec < 2:
+            return 'Thunder and rain'
+        else:
+            return 'Thunder and heavy rain'
+    elif icon is 101:
+        return 'Clear sky'
+
 
 
 def format_line(data: str) -> str:
     weather = json.loads(data)
     current_hour = weather['timeserie'][0]
-    precipitation = float(current_hour['precip1'])
-    weather_description = format_weather_desc(precipitation)
-    precipitation = f'{round(precipitation, 1)}mm ' if precipitation > 0.0 else ""
+    precipitation = round(float(current_hour['precip1']), 1)
+    weather_description = format_weather_desc(precipitation, int(current_hour['symbol']))
+    precipitation = f'{precipitation}mm ' if precipitation > 0.0 else ""
     temperature = int(current_hour['temp'])
     humidity = int(current_hour['humidity'])
     wind_direction = format_wind_dir(current_hour['windDir'])
