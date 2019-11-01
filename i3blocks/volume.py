552fd0e86e7d97b3051ea1b8b42ac06fc0b05d82
Badgy <jonaskhansen@gmail.com>
2019-11-01 12:54:23 +0100

    use pulseaudio ctl rather than pamixer

--- i3blocks/volume.py
+++ i3blocks/volume.py
@@ -3,12 +3,12 @@
 import subprocess
 import os
 
-volume = subprocess.run(['pamixer', '--get-volume'], stdout=subprocess.PIPE).stdout.decode('utf-8').replace('\n', '')
-mute = subprocess.run(['pamixer', '--get-mute'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+# [vol, sink-mute, src-mute]
+status = subprocess.run(['pulseaudio-ctl', 'full-status'], stdout=subprocess.PIPE).stdout.decode('utf-8').split(' ')
 
-if 'false' in mute:
-    print(f' {volume}%')
-elif 'true' in mute:
+if 'no' in status[1]:
+    print(f' {status[0]}%')
+else:
     os.system(f'echo " M"')
     os.system(f'echo ')
     os.system(f'echo "#F4FA58"')
