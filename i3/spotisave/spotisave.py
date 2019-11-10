a403a9e69637000aa57f90380b1f53c214a32858
Badgy <jonaskhansen@gmail.com>
2019-11-10 23:53:41 +0100

    add spotisave

--- i3/spotisave/spotisave.py
+++ i3/spotisave/spotisave.py
@@ -2,15 +2,44 @@
 
 import requests
 import subprocess
-import configparser
+import webbrowser
+from bottle import route, run, request
+from spotipy import oauth2
 from pathlib import Path
 
-config = configparser.ConfigParser()
-config.read(f'{Path.home()}/.config/i3/spotisave/config')
-token = config['spotify']['token']
-headers = {'Content-Type': 'application/json',
-           'Authorization': f'Bearer {token}'}
-playlist_id = config['spotify']['playlist']
+port = 8080
+client_id = '466a89a53359403b82df7d714030ec5f'
+client_secret = '28147de72c3549e98b1e790f3d080b85'
+redirect_uri = f'http://localhost:{port}'
+scope = 'playlist-modify-public%20playlist-modify-private'
+cache = f'{Path.home()}/.spotisaveoauth'
+playlist_id = "783TdjdiLozExUlSFcMWE4"
+
+sp_oauth = oauth2.SpotifyOAuth(client_id, client_secret, redirect_uri, scope=scope, cache_path=cache)
+
+
+def authorize():
+    webbrowser.open(redirect_uri)
+    run(host='', port=port)
+
+
+@route('/')
+def index() -> str:
+    access_token = ""
+    url = request.url
+    code = sp_oauth.parse_response_code(url)
+    if code:
+        token_info = sp_oauth.get_access_token(code)
+        access_token = token_info['access_token']
+
+    if access_token:
+        return "<span>Successfully retrieved OAuth token. You may close this tab and start using Spotisave.</span>"
+    else:
+        return f"<a href='{sp_oauth.get_authorize_url()}'>Login to Spotify</a>"
+
+
+def refresh_token():
+    sp_oauth.refresh_access_token(sp_oauth.get_cached_token()['refresh_token'])
 
 
 def get_current_song_uri() -> str:
@@ -18,12 +47,27 @@ def get_current_song_uri() -> str:
         .stdout.decode('utf-8').strip('\n')
 
 
+def get_token() -> str:
+    token_info = sp_oauth.get_cached_token()
+    if token_info:
+        return sp_oauth.get_cached_token()['access_token']
+    else:
+        authorize()
+        exit(1)
+
+
 def add_song():
+    headers = {'Content-Type': 'application/json',
+               'Authorization': f'Bearer {get_token()}'}
     params = f'uris={get_current_song_uri()}'
     response = requests.post(f'https://api.spotify.com/v1/playlists/{playlist_id}/tracks', headers=headers,
                              params=params)
     if response.status_code == 201:
         print('Successfully added song to playlist')
+    elif response.status_code == 401:
+        print('OAuth token invalid, refreshing...')
+        refresh_token()
+        add_song()
     else:
         print(f'Failed to add song to playlist, status code: {response.status_code}')
         print(response.content.decode('utf-8'))
