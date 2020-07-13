f6653448181e9c8e5fa8e34ef1c202a05aa94a11
Badgie <jonaskhansen@gmail.com>
2020-07-13 13:30:45 +0200

    update

--- setup.sh
+++ setup.sh
@@ -11,6 +11,47 @@ sys_symlink() {
     sudo ln -s "$(realpath ./"$1")" "$2"
 }
 
+# initial necessities
+# needs manual install
+# sudo pacman -S base-devel sudo vim netctl wpa_supplicant dialog
+
+# basic desktop
+echo 'setting up basic desktop'
+sudo pacman -S i3-gaps i3blocks i3status xorg-server lightdm lightdm-gtk-greeter konsole
+
+# utility
+# ntfs-3g: allow write on usb drives
+sudo pacman -S ntfs-3g
+
+# yay
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/yay.tar.gz
+mkdir build
+mv yay.tar.gz build
+cd build
+tar -xvf yay.tar.gz
+cd yay
+makepkg -si
+cd
+
+# enable multilib
+sudo sed -i "s/#[multilib]/[multilib]/g" /etc/pacman.conf
+sudo sed -i "s/#Include = /etc/pacman.d/mirrorlist/Include = /etc/pacman.d/mirrorlist/g" /etc/pacman.conf
+
+# install remaining
+# xclip: clipboard utility for i3 binds
+# upower: utility for battery block in i3
+yay -S spotirec-git chwifi-git escrotum-git pycharm-community-edition xclip wine \
+       whatsapp-nativefier vlc upower typora ttf-dejavu ttf-hanazono ttf-liberation trello \
+       tixati steam-fonts steam spotify spicetify-cli smartmontools slack-desktop rofi pulseaudio
+       playerctl parole p7zip obs-studio noto-fonts noto-fonts-emoji nmap neofetch nemo lutris
+       inkscape htop gimp freetype2 firefox feh evince dunst drun docker discord cloc caprine
+       calibre blender betterlockscreen arc-gtk-theme-git \
+
+# todo
+# usb drive:
+#   wp dir to /home/usr/wp
+
+
 symlink "betterlockscreen/betterlockscreenrc" ".config/betterlockscreenrc"
 
 mkdir -p "$HOME/scripts/lock"
@@ -35,8 +76,9 @@ symlink "i3blocks/config" ".config/i3blocks/config"
 
 mkdir -p "$HOME/.config/i3"
 symlink "i3/config" ".config/i3/config"
-mkdir -p "$HOME/.config/i3/spotisave"
-symlink "i3/spotisave/spotisave.py" ".config/i3/spotisave/spotisave.py"
+
+mkdir -p "$HOME/.config/dunst"
+symlink "dunst/dunstrc" ".config/dunst/dunstrc"
 
 sys_symlink "shufflewall/shufflewall.service" "/etc/systemd/system/shufflewall.service"
 sys_symlink "shufflewall/shufflewall.timer" "/etc/systemd/system/shufflewall.timer"
